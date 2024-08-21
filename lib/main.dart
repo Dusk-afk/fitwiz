c5e09836273472d9fe3c59ac9d7a21d05202d175
Piyush <piyushsvps@gmail.com>
2024-08-22 01:27:30 +0530

    cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,6 @@ import 'package:fitwiz/features/auth/presentation/screens/login_screen.dart';
 import 'package:fitwiz/features/auth/presentation/screens/register_screen.dart';
 import 'package:fitwiz/features/main/presentation/screens/main_screen.dart';
 import 'package:fitwiz/features/splash/presentation/screens/splash_screen.dart';
-import 'package:fitwiz/utils/components/custom_back_button.dart';
-import 'package:fitwiz/utils/components/custom_bottom_sheet.dart';
-import 'package:fitwiz/utils/components/custom_button.dart';
-import 'package:fitwiz/utils/components/custom_icon.dart';
-import 'package:fitwiz/utils/components/custom_notifications.dart';
-import 'package:fitwiz/utils/components/custom_search_field.dart';
-import 'package:fitwiz/utils/components/custom_text_field.dart';
 import 'package:fitwiz/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -50,55 +43,6 @@ class MainApp extends StatelessWidget {
             "/login": (context) => const LoginScreen(),
             "/register": (context) => const RegisterScreen(),
           },
-          // home: Scaffold(
-          //   body: Builder(builder: (context) {
-          //     return Column(
-          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //       children: [
-          //         CustomButton(
-          //           onPressed: () {
-          //             CustomNotifications.notifySuccess(
-          //               context: context,
-          //               message: "An event has been created successfuly",
-          //             );
-          //           },
-          //           label: "Hello",
-          //         ),
-          //         const CustomButton(
-          //           onPressed: null,
-          //           label: "Hello",
-          //         ),
-          //         CustomButton.secondary(
-          //           onPressed: () {
-          //             CustomBottomSheet.showSimpleSheet(
-          //               icon: CustomIcon(
-          //                 CustomIcons.tick,
-          //                 size: 16.sp,
-          //               ),
-          //               title: "Event created",
-          //               message: "An event has been created successfuly",
-          //             );
-          //           },
-          //           label: "Hello",
-          //         ),
-          //         const CustomButton.secondary(
-          //           onPressed: null,
-          //           label: "Hello",
-          //         ),
-          //         CustomBackButton(onPressed: () {
-          //           CustomBottomSheet.showSimpleLoadingSheet(message: "Loading");
-          //         }),
-          //         CustomIcon(
-          //           CustomIcons.tick,
-          //           size: 16.sp,
-          //           containerSize: 24.sp,
-          //         ),
-          //         const CustomTextField(),
-          //         const CustomSearchField(),
-          //       ],
-          //     );
-          //   }),
-          // ),
         ),
       ),
     );
